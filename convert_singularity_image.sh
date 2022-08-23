3c41c1399af37593c911d62b8c4d4edb21d4eec3
Leonardo Cristella <leonardo.cristella@epfl.ch>
2022-08-23 13:03:51 +0200

    no exec test

--- convert_singularity_image.sh
+++ convert_singularity_image.sh
@@ -27,21 +27,23 @@ export SINGULARITY_CACHEDIR=${TMPDIR}/singularity-cachedir
 export SINGULARITY_DOCKER_USERNAME=${CI_REGISTRY_USER}
 export SINGULARITY_DOCKER_PASSWORD=${CI_JOB_TOKEN}
 echo "Pulling the image from the GitLab registry:"
-singularity pull --no-https ${TMPDIR}/blue_brain_atlas_pipeline.sif docker://bbpgitlab.epfl.ch:5050/dke/apps/blue_brain_atlas_pipeline:latest
+image="blue_brain_atlas_pipeline"
+imagesif="$image.sif"
+singularity pull --no-https ${TMPDIR}/$imagesif docker://bbpgitlab.epfl.ch:5050/dke/apps/$image:latest
 
 echo "At this stage, we have the singularity image at ${TMPDIR}/blue_brain_atlas_pipeline.sif"
-ls -la ${TMPDIR}/blue_brain_atlas_pipeline.sif
+ls -la ${TMPDIR}/$imagesif
 
-echo "Run tests: as a demo, just check if we can get the help of 2048"
-singularity exec --containall ${TMPDIR}/blue_brain_atlas_pipeline.sif 2048 -h
+#echo "Run tests: as a demo, just check if we can get the help of 2048"
+#singularity exec --containall ${TMPDIR}/blue_brain_atlas_pipeline.sif 2048 -h
 
 echo "Deploying the image to proj83"
 export GPFSDIR=/gpfs/bbp.cscs.ch/data/project/proj83/singularity-images
-export NAME_WITH_TIMESTAMP=blue_brain_atlas_pipeline-$(date +%Y-%m-%dT%H:%M:%S).sif
+export NAME_WITH_TIMESTAMP=$image-$(date +%Y-%m-%dT%H:%M:%S).sif
 mkdir -p ${GPFSDIR}
-mv ${TMPDIR}/blue_brain_atlas_pipeline.sif ${GPFSDIR}/${NAME_WITH_TIMESTAMP}
-rm -f ${GPFSDIR}/blue_brain_atlas_pipeline.sif
-ln -s ${GPFSDIR}/${NAME_WITH_TIMESTAMP} ${GPFSDIR}/blue_brain_atlas_pipeline.sif
+mv ${TMPDIR}/$imagesif ${GPFSDIR}/${NAME_WITH_TIMESTAMP}
+rm -f ${GPFSDIR}/$imagesif
+ln -s ${GPFSDIR}/${NAME_WITH_TIMESTAMP} ${GPFSDIR}/$imagesif
 
 echo "Updated ${GPFSDIR}/blue_brain_atlas_pipeline.sif which is actually a symbolic link to ${GPFSDIR}/${NAME_WITH_TIMESTAMP}"
 
