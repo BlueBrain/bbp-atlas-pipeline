0b5822a38bf536591f89e208540938919ab3c72c
Leonardo Cristella <leonardo.cristella@epfl.ch>
2024-03-02 12:33:33 +0100

    user_config_path = config[USER_CONFIG]

--- bbp_atlas_cli.py
+++ bbp_atlas_cli.py
@@ -10,13 +10,13 @@ import re
               type=click.STRING, required=False,
               help="The target rule of the pipeline to execute (required without a user configuration)")
 @click.option("--user-config-file",
-              type=click.Path(exists=True), required=False,
+              type=click.Path(), required=False,
               help="The user configuration to customize the pipeline")
 @click.option("--repo-path",
               type=click.Path(exists=True), required=False, default=".",
               help="The path of the pipeline repository")
 @click.option("--snakemake-options",
-              type=click.STRING, required=False,
+              type=click.STRING, required=False, default="",
               help="String of options to pass to the snakemake command")
 @click.option("--service-token",
               is_flag=True, default=False,
@@ -35,6 +35,10 @@ def execute_pipeline(target_rule, user_config_file, repo_path, snakemake_options
         if not target_rule:
             raise Exception("A target rule is required if no user configuration is provided")
     else:
+        user_config_path = os.path.join(repo_path, user_config_file)
+        if not os.path.isfile(user_config_path):
+            raise FileNotFoundError(f"--user-config-file {user_config_path} not found.")
+
         if target_rule:
             raise Exception(f"When a user configuration is provided (--user-config-file {user_config_file}),"
                             " the target rule must be specified in the user configuration"
@@ -49,7 +53,7 @@ def execute_pipeline(target_rule, user_config_file, repo_path, snakemake_options
 
         with open(os.path.join(repo_path, "config.yaml")) as pipeline_config_file:
             pipeline_config = yaml.safe_load(pipeline_config_file.read().strip())
-        keycloak_config = pipeline_config["KEYCLOAK_CONFIG"]
+        keycloak_config = os.path.join(repo_path, pipeline_config["KEYCLOAK_CONFIG"])
         working_dir = pipeline_config["WORKING_DIR"]
         if not service_token:
             token_fetcher = TokenFetcherUser(token_username, token_password, keycloak_config_file=keycloak_config)
@@ -67,7 +71,7 @@ def execute_pipeline(target_rule, user_config_file, repo_path, snakemake_options
 
         pipeline_validator(user_config_file, token_fetcher.get_access_token(), whitelisted_vars)
 
-        user_config_json = json.load(open(os.path.join(repo_path, user_config_file)))
+        user_config_json = json.load(open(user_config_path))
         target_rule = user_config_json["target_rule"]
         priority_rules = []
         for user_rule_name in [user_rule["rule"] for user_rule in user_config_json["rules"]]:
@@ -77,13 +81,20 @@ def execute_pipeline(target_rule, user_config_file, repo_path, snakemake_options
             else:
                 priority_rules.append(merge_rule_name)
 
-        pipeline_command += f" --snakefile {repo_path}/customize_pipeline/custom_snakefile"
+        snakemake_options += f" --snakefile {repo_path}/customize_pipeline/custom_snakefile"
         if priority_rules:
-            pipeline_command += f" --prioritize {' '.join(priority_rules)}"
+            snakemake_options += f" --prioritize {' '.join(priority_rules)}"
+
+        user_config_option = f"USER_CONFIG={user_config_path}"
+        full_user_config_option = f"--config {user_config_option}"
+        if "--config" in snakemake_options:
+            snakemake_options = snakemake_options.replace("--config", full_user_config_option)
+        else:
+            snakemake_options = "  ".join([full_user_config_option, snakemake_options])
 
     full_snakemake_options = " --printshellcmds"
     if snakemake_options:
-        full_snakemake_options += " " + snakemake_options
+        full_snakemake_options = " ".join([full_snakemake_options, snakemake_options])
 
     pipeline_command += " ".join([full_snakemake_options, target_rule])
     print("\nExecuting command:\n", pipeline_command)
