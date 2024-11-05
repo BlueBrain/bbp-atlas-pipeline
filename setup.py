de8908fb2d8677c057aa69ff2a23a6860208ff66
Leonardo Cristella <leonardo.cristella@epfl.ch>
2024-11-05 18:00:08 +0100

    Upgrade to python 3.10 for blue-cwl dependency

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/BlueBrain/bbp-atlas-pipeline",
     license="Apache-2.0",
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     install_requires=[
         "nexusforge>=0.8.2",
         "click>=7.0",
